f6dbc19921394e9ccba0abb4f77fbf19210ba730
dbt explorer <explorerdbt@gmail.com>
2025-12-31 11:18:44 +0000

    update

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -8,7 +8,9 @@ select
     c.c_acctbal,
     o.o_orderstatus,
     o.o_orderdate,
-    o.o_totalprice
+    o.o_totalprice,
+    s.region
 from {{ ref("stg_customers") }} c
 left join {{ ref("fct_sales") }} o on c.c_custkey = o.o_custkey
+left join {{ref('sales')}} s on o_orderstatus = s.status_code
 where o.o_custkey is not null
